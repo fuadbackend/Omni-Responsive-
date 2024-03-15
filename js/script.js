ab852198ca035fdaaec6517ea5f383af980863cb
fuadbackend <fuadbackend@gmail.com>
2024-03-15 19:02:16 +0600

    hi deploy

--- js/script.js
+++ js/script.js
@@ -20,6 +20,8 @@ const allLinks = document.querySelectorAll("a:link");
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
+    {}
+    
     const href = link.getAttribute("href");
     console.log(href);
 
